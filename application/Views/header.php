4f64877eb3c338d0c80351bdc5b6a6bf967b5a31
Syakirin Amin <akrinmin@gmail.com>
2018-06-09 00:18:18 +0700

    Fb login button

--- application/Views/header.php
+++ application/Views/header.php
@@ -85,7 +85,9 @@ helper('url');
               </a>
               <div class="d-flex order-lg-2 ml-auto">
 
-
+				<div class="nav-item d-md-flex">
+                  <a href="/fb-login" class="btn btn-sm btn-outline-primary">Login with FB</a>
+                </div>
               </div>
               <a href="#" class="header-toggler d-lg-none ml-3 ml-lg-0" data-toggle="collapse" data-target="#headerMenuCollapse">
                 <span class="header-toggler-icon"></span>
